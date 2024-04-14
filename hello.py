daba5cf21ac45c3ad2ed707629923281f31e71a6
pnngnas <huusangvtvip@gmail.com>
2024-04-14 10:24:29 +0700

    s

--- hello.py
+++ hello.py
@@ -1,22 +1,38 @@
 from flask import Flask, request, send_file
 import os
+import pickle as pk
+import time
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+import subprocess
 app = Flask(__name__)
 
 
+
+model1 = load_model('cnn.h5')
 
 UPLOAD_FOLDER = './uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/', methods=['POST'])
 def upload_file():
+    file = request.files['file']
     if file:
-        filename = file.filename
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        newname = filename.replace(".pcap", "")
-        with open("./uploads/" + newname + '.csv', 'w') as file:
-            file.write("skdlsd")
+        try:
+            filename = file.filename
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            newname = filename.replace(".pcap", "")
+            
 
-        statement = ['cicflowmeter', '-f' , "./uploads/" + filename, '-c', "./uploads/" + newname + '.csv']
-        return send_file(os.path.join(app.config['UPLOAD_FOLDER'], newname + '.csv'), as_attachment=True)
+            statement = ['cicflowmeter', '-f' , "./uploads/" + filename, '-c', "./uploads/" + newname + '.csv']
+            subprocess.run(statement, check=True)
+            a = model.predict("./uploads/" + newname + '.csv')
+            time.sleep(1)
+            return a
+            return send_file(os.path.join(app.config['UPLOAD_FOLDER'], newname + '.csv'), as_attachment=True)
+        
+        except Exception:
+            return "Error"
+            
 
         
