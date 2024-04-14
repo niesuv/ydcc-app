c32b0fc6ff184e965f47c20d56b837cebb196af5
pnngnas <huusangvtvip@gmail.com>
2024-04-14 11:46:35 +0700

    p

--- hello.py
+++ hello.py
@@ -5,8 +5,93 @@ import time
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 import subprocess
+import pandas as pd
 app = Flask(__name__)
 
+def change_feature_name(dataset):
+  array = [i for i in range(len(dataset.columns))]
+  for i in range(len(dataset.columns.values)):
+    array[i] = dataset.columns.values[i]
+    array[i] = array[i].replace("_", " ")
+    array[i] = array[i].replace("src", "Source")
+    array[i] = array[i].replace("dst", "Destination")
+    array[i] = array[i].replace("ip", "IP")
+    array[i] = array[i].replace("tot", "Total")
+    array[i] = array[i].replace("pkts", "Packets")
+    array[i] = array[i].replace("pkt", "Packet")
+    array[i] = array[i].replace("cnt", "Count")
+    array[i] = array[i].replace("seg", "Segment")
+    array[i] = array[i].replace("var", "Variance")
+    array[i] = array[i].replace("len", "length")
+    array[i] = array[i].replace("byts", "Bytes")
+    array[i] = array[i].title()
+    array[i] = array[i].replace("Ip", "IP")
+    array[i] = array[i].replace("Iat", "IAT")
+    array[i] = array[i].replace("Fin", "FIN")
+    array[i] = array[i].replace("Ack", "ACK")
+    array[i] = array[i].replace("Syn", "SYN")
+    array[i] = array[i].replace("Psh", "PSH")
+    array[i] = array[i].replace("Rst", "RST")
+    array[i] = array[i].replace("Ece", "ECE")
+    array[i] = array[i].replace("Cwe", "CWE")
+    array[i] = array[i].replace("Urg", "URG")
+    array[i] = array[i].replace(" B ", " Bulk ")
+    array[i] = array[i].replace("s S", "s/s")
+    array[i] = array[i].replace("Blk", "Bulk")
+    array[i] = array[i].replace("Totallength", "Total Length of")
+    array[i] = array[i].replace("Down Up Ratio", "Down/Up Ratio")
+    array[i] = array[i].replace("Total Bwd Packets", "Total Backward Packets")
+    array[i] = array[i].replace("Packet Length Max", "Max Packet Length")
+    array[i] = array[i].replace("Packet Length Min", "Min Packet Length")
+    array[i] = array[i].replace("Fwd Max Packet Length", "Fwd Packet Length Max")
+    array[i] = array[i].replace("Fwd Min Packet Length", "Fwd Packet Length Min")
+    array[i] = array[i].replace("Bwd Max Packet Length", "Bwd Packet Length Max")
+    array[i] = array[i].replace("Bwd Min Packet Length", "Bwd Packet Length Min")
+    array[i] = array[i].replace("Fwd Segment Size Min", "min_seg_size_forward")
+    array[i] = array[i].replace("Fwd Act Data Packets", "act_data_pkt_fwd")
+    array[i] = array[i].replace("Packet Size Avg", "Average Packet Size")
+    array[i] = array[i].replace("Init Fwd Win Bytes", "Init_Win_bytes_forward")
+    array[i] = array[i].replace("Init Bwd Win Bytes", "Init_Win_bytes_backward")
+    array[i] = array[i].replace("Fwd Segment Size Avg", "Avg Fwd Segment Size")
+    array[i] = array[i].replace("Bwd Segment Size Avg", "Avg Bwd Segment Size")
+    array[i] = array[i].replace("Fwd Bulk Rate Avg", "Fwd Avg Bulk Rate")
+    array[i] = array[i].replace("Bwd Bulk Rate Avg", "Bwd Avg Bulk Rate")
+    array[i] = array[i].replace("Fwd Bytes Bulk Avg", "Fwd Avg Bytes/Bulk")
+    array[i] = array[i].replace("Fwd Packets Bulk Avg", "Fwd Avg Packets/Bulk")
+    array[i] = array[i].replace("Bwd Bytes Bulk Avg", "Bwd Avg Bytes/Bulk")
+    array[i] = array[i].replace("Bwd Packets Bulk Avg", "Bwd Avg Packets/Bulk")
+  print(len(array))
+  return array
+feature_list = ['Destination Port', 'Flow Duration', 'Total Fwd Packets','Total Backward Packets', 'Total Length of Fwd Packets', 'Total Length of Bwd Packets', 'Fwd Packet Length Max',
+       'Fwd Packet Length Min', 'Fwd Packet Length Mean',
+       'Fwd Packet Length Std', 'Bwd Packet Length Max',
+       'Bwd Packet Length Min', 'Bwd Packet Length Mean',
+       'Bwd Packet Length Std', 'Flow IAT Mean', 'Flow IAT Std',
+       'Flow IAT Max', 'Flow IAT Min', 'Fwd IAT Total', 'Fwd IAT Mean',
+       'Fwd IAT Std', 'Fwd IAT Max', 'Fwd IAT Min', 'Bwd IAT Total',
+       'Bwd IAT Mean', 'Bwd IAT Std', 'Bwd IAT Max', 'Bwd IAT Min',
+       'Fwd PSH Flags', 'Bwd PSH Flags', 'Fwd URG Flags', 'Bwd URG Flags',
+       'Fwd Header Length', 'Bwd Header Length', 'Fwd Packets/s',
+       'Bwd Packets/s', 'Min Packet Length', 'Max Packet Length',
+       'Packet Length Mean', 'Packet Length Std', 'Packet Length Variance',
+       'FIN Flag Count', 'SYN Flag Count', 'RST Flag Count', 'PSH Flag Count',
+       'ACK Flag Count', 'URG Flag Count', 'CWE Flag Count', 'ECE Flag Count',
+       'Down/Up Ratio', 'Average Packet Size', 'Avg Fwd Segment Size',
+       'Avg Bwd Segment Size', 'Fwd Avg Bytes/Bulk', 'Fwd Avg Packets/Bulk',
+       'Fwd Avg Bulk Rate', 'Bwd Avg Bytes/Bulk', 'Bwd Avg Packets/Bulk',
+       'Bwd Avg Bulk Rate', 'Subflow Fwd Packets', 'Subflow Fwd Bytes',
+       'Subflow Bwd Packets', 'Subflow Bwd Bytes', 'Init_Win_bytes_forward',
+       'Init_Win_bytes_backward', 'act_data_pkt_fwd', 'min_seg_size_forward',
+       'Active Mean', 'Active Std', 'Active Max', 'Active Min', 'Idle Mean',
+       'Idle Std', 'Idle Max', 'Idle Min']
+
+def drop_feature(dataset):
+  dataset.drop("Flow Bytes/s", axis=1, inplace=True)
+  dataset.drop("Flow Packets/s", axis=1, inplace=True)
+  cols = dataset.columns.values
+  for col in cols:
+    if col not in feature_list:
+      dataset.drop(col, axis=1, inplace=True)
 
 
 model1 = load_model('cnn.h5')
@@ -22,23 +107,31 @@ def upload_file():
             filename = file.filename
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             newname = filename.replace(".pcap", "")
-            
+
 
             statement = ['cicflowmeter', '-f' , "./uploads/" + filename, '-c', "./uploads/" + newname + '.csv']
             subprocess.run(statement, check=True)
-            a = model.predict("./uploads/" + newname + '.csv')
-            time.sleep(1)
-            if os.path.exists("./uploads/" + newname + '.csv'):
-                os.remove("./uploads/" + newname + '.csv')
-            
-            if os.path.exists("./uploads/" + filename ):
-                os.remove("./uploads/" + newname)
-            return a
+            time.sleep(5)
+            X = pd.read_csv("./uploads/" + newname + '.csv')
+            X.columns = change_feature_name(X)
+            drop_feature(X)
+            X = X.reindex(columns=feature_list)
+            pca_reload = pk.load(open("pca.pkl",'rb'))
+            result_new = pca_reload.transform(X)
+            #pca_reload = pk.load(open("pca.pkl",'rb'))
+            #result_new = pca_reload .transform(X)
+            a = model1.predict(result_new)
+            time.sleep(3)
+            a = a.flatten().tolist()
+            num1 = sum(a)
+            num0 = len(a) - num1
+            if num1 > num0:
+                return "safe"
+            return "can be dos ed"
             return send_file(os.path.join(app.config['UPLOAD_FOLDER'], newname + '.csv'), as_attachment=True)
-        
-        except Exception:
-            print(E)
-            return "Error"
-            
 
-        
+        except Exception as e:
+            print(e)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8000, debug=True)
--- py.py
+++ py.py
@@ -1,137 +1,87 @@
-from flask import Flask, request, send_file
-import os
-import pickle as pk
-import time
-import tensorflow as tf
-from tensorflow.keras.models import load_model
+import os, signal
+import configparser
+import schedule
 import subprocess
-import pandas as pd
-app = Flask(__name__)
-
-def change_feature_name(dataset):
-  array = [i for i in range(len(dataset.columns))]
-  for i in range(len(dataset.columns.values)):
-    array[i] = dataset.columns.values[i]
-    array[i] = array[i].replace("_", " ")
-    array[i] = array[i].replace("src", "Source")
-    array[i] = array[i].replace("dst", "Destination")
-    array[i] = array[i].replace("ip", "IP")
-    array[i] = array[i].replace("tot", "Total")
-    array[i] = array[i].replace("pkts", "Packets")
-    array[i] = array[i].replace("pkt", "Packet")
-    array[i] = array[i].replace("cnt", "Count")
-    array[i] = array[i].replace("seg", "Segment")
-    array[i] = array[i].replace("var", "Variance")
-    array[i] = array[i].replace("len", "length")
-    array[i] = array[i].replace("byts", "Bytes")
-    array[i] = array[i].title()
-    array[i] = array[i].replace("Ip", "IP")
-    array[i] = array[i].replace("Iat", "IAT")
-    array[i] = array[i].replace("Fin", "FIN")
-    array[i] = array[i].replace("Ack", "ACK")
-    array[i] = array[i].replace("Syn", "SYN")
-    array[i] = array[i].replace("Psh", "PSH")
-    array[i] = array[i].replace("Rst", "RST")
-    array[i] = array[i].replace("Ece", "ECE")
-    array[i] = array[i].replace("Cwe", "CWE")
-    array[i] = array[i].replace("Urg", "URG")
-    array[i] = array[i].replace(" B ", " Bulk ")
-    array[i] = array[i].replace("s S", "s/s")
-    array[i] = array[i].replace("Blk", "Bulk")
-    array[i] = array[i].replace("Totallength", "Total Length of")
-    array[i] = array[i].replace("Down Up Ratio", "Down/Up Ratio")
-    array[i] = array[i].replace("Total Bwd Packets", "Total Backward Packets")
-    array[i] = array[i].replace("Packet Length Max", "Max Packet Length")
-    array[i] = array[i].replace("Packet Length Min", "Min Packet Length")
-    array[i] = array[i].replace("Fwd Max Packet Length", "Fwd Packet Length Max")
-    array[i] = array[i].replace("Fwd Min Packet Length", "Fwd Packet Length Min")
-    array[i] = array[i].replace("Bwd Max Packet Length", "Bwd Packet Length Max")
-    array[i] = array[i].replace("Bwd Min Packet Length", "Bwd Packet Length Min")
-    array[i] = array[i].replace("Fwd Segment Size Min", "min_seg_size_forward")
-    array[i] = array[i].replace("Fwd Act Data Packets", "act_data_pkt_fwd")
-    array[i] = array[i].replace("Packet Size Avg", "Average Packet Size")
-    array[i] = array[i].replace("Init Fwd Win Bytes", "Init_Win_bytes_forward")
-    array[i] = array[i].replace("Init Bwd Win Bytes", "Init_Win_bytes_backward")
-    array[i] = array[i].replace("Fwd Segment Size Avg", "Avg Fwd Segment Size")
-    array[i] = array[i].replace("Bwd Segment Size Avg", "Avg Bwd Segment Size")
-    array[i] = array[i].replace("Fwd Bulk Rate Avg", "Fwd Avg Bulk Rate")
-    array[i] = array[i].replace("Bwd Bulk Rate Avg", "Bwd Avg Bulk Rate")
-    array[i] = array[i].replace("Fwd Bytes Bulk Avg", "Fwd Avg Bytes/Bulk")
-    array[i] = array[i].replace("Fwd Packets Bulk Avg", "Fwd Avg Packets/Bulk")
-    array[i] = array[i].replace("Bwd Bytes Bulk Avg", "Bwd Avg Bytes/Bulk")
-    array[i] = array[i].replace("Bwd Packets Bulk Avg", "Bwd Avg Packets/Bulk")
-  print(len(array))
-  return array
-feature_list = ['Destination Port', 'Flow Duration', 'Total Fwd Packets','Total Backward Packets', 'Total Length of Fwd Packets', 'Total Length of Bwd Packets', 'Fwd Packet Length Max',
-       'Fwd Packet Length Min', 'Fwd Packet Length Mean',
-       'Fwd Packet Length Std', 'Bwd Packet Length Max',
-       'Bwd Packet Length Min', 'Bwd Packet Length Mean',
-       'Bwd Packet Length Std', 'Flow IAT Mean', 'Flow IAT Std',
-       'Flow IAT Max', 'Flow IAT Min', 'Fwd IAT Total', 'Fwd IAT Mean',
-       'Fwd IAT Std', 'Fwd IAT Max', 'Fwd IAT Min', 'Bwd IAT Total',
-       'Bwd IAT Mean', 'Bwd IAT Std', 'Bwd IAT Max', 'Bwd IAT Min',
-       'Fwd PSH Flags', 'Bwd PSH Flags', 'Fwd URG Flags', 'Bwd URG Flags',
-       'Fwd Header Length', 'Bwd Header Length', 'Fwd Packets/s',
-       'Bwd Packets/s', 'Min Packet Length', 'Max Packet Length',
-       'Packet Length Mean', 'Packet Length Std', 'Packet Length Variance',
-       'FIN Flag Count', 'SYN Flag Count', 'RST Flag Count', 'PSH Flag Count',
-       'ACK Flag Count', 'URG Flag Count', 'CWE Flag Count', 'ECE Flag Count',
-       'Down/Up Ratio', 'Average Packet Size', 'Avg Fwd Segment Size',
-       'Avg Bwd Segment Size', 'Fwd Avg Bytes/Bulk', 'Fwd Avg Packets/Bulk',
-       'Fwd Avg Bulk Rate', 'Bwd Avg Bytes/Bulk', 'Bwd Avg Packets/Bulk',
-       'Bwd Avg Bulk Rate', 'Subflow Fwd Packets', 'Subflow Fwd Bytes',
-       'Subflow Bwd Packets', 'Subflow Bwd Bytes', 'Init_Win_bytes_forward',
-       'Init_Win_bytes_backward', 'act_data_pkt_fwd', 'min_seg_size_forward',
-       'Active Mean', 'Active Std', 'Active Max', 'Active Min', 'Idle Mean',
-       'Idle Std', 'Idle Max', 'Idle Min']
-
-def drop_feature(dataset):
-  dataset.drop("Flow Bytes/s", axis=1, inplace=True)
-  dataset.drop("Flow Packets/s", axis=1, inplace=True)
-  cols = dataset.columns.values
-  for col in cols:
-    if col not in feature_list:
-      dataset.drop(col, axis=1, inplace=True)
+import time
+import requests
+from datetime import datetime
 
 
-model1 = load_model('cnn.h5')
+def doreq(filename):
+    url = "35.240.246.167:8000/"
 
-UPLOAD_FOLDER = './uploads'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+    payload = {}
+    files=[
+    ('file',('syn.pcap',open(filename,'rb'),'application/octet-stream'))
+    ]
+    headers = {}
 
-@app.route('/', methods=['POST'])
-def upload_file():
-    file = request.files['file']
-    if file:
-        try:
-            filename = file.filename
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            newname = filename.replace(".pcap", "")
+    response = requests.request("POST", url, headers=headers, data=payload, files=files)
+    with open("history.txt" , '+a') as file:
+        file.write("\n" + response.t)
+    
+
+lastpid = -1
+lasttime = 0
+config = configparser.ConfigParser()
+config.read("config.ini")
+def job():
+    global lastpid
+    try:
+        if lastpid != -1:
+            os.kill(lastpid, signal.SIGTERM)
+        datenow = datetime.now().strftime(r"%Y-%m-%d %H:%M:%S")
+        statement = ['tcpdump', '-i', 'any' ,'-nn', '-c' , str(packet) , '-w' ,datenow + '.pcap'] 
+        process =  subprocess.Popen(statement, shell=True)
+        lastpid = process.pid
+        if lasttime != 0:
+            doreq(datenow + ".pcap")    
+            
+
+        
+    except ProcessLookupError:
+        pass
+    except PermissionError:
+        pass    
+    except OSError:
+        datenow = datetime.now().strftime(r"%Y-%m-%d %H:%M:%S")
+        statement = ['tcpdump', '-i', 'any' ,'-nn', '-c' , str(packet) , '-w' ,datenow + '.pcap'] 
+        statement = ['dir']
+        process =  subprocess.Popen(statement, shell=True)
+        lastpid = process.pid
+        if lasttime != 0:
+            doreq(datenow + ".pcap") 
+
+number = 1
+by = 'day'
+packet = 1000
+if 'SCHEDULE' in config and "number" in config['SCHEDULE']:
+    number = int(config["SCHEDULE"]["number"])
+
+if 'SCHEDULE' in config and "by" in config['SCHEDULE']:
+    by = config["SCHEDULE"]["by"]
+
+if 'EACH' in config and "packet" in config['EACH']:
+    packet = int(config["EACH"]["packet"])
 
 
-            statement = ['cicflowmeter', '-f' , "./uploads/" + filename, '-c', "./uploads/" + newname + '.csv']
-            subprocess.run(statement, check=True)
-            time.sleep(5)
-            X = pd.read_csv("./uploads/" + newname + '.csv')
-            X.columns = change_feature_name(X)
-            drop_feature(X)
-            X = X.reindex(columns=feature_list)
-            pca_reload = pk.load(open("pca.pkl",'rb'))
-            result_new = pca_reload.transform(X)
-            #pca_reload = pk.load(open("pca.pkl",'rb'))
-            #result_new = pca_reload .transform(X)
-            a = model1.predict(result_new)
-            time.sleep(3)
-            a = a.flatten().tolist()
-            num1 = sum(a)
-            num0 = len(a) - num1
-            if num1 > num0:
-                return "safe"
-            return "can be dos ed"
-            return send_file(os.path.join(app.config['UPLOAD_FOLDER'], newname + '.csv'), as_attachment=True)
+if by == 'day':
+    schedule.every(number).days.do(job)
+if by == 'hour':
+    schedule.every(number).hours.do(job)
+if by == 'minute':
+    schedule.every(number).minutes.do(job)
+if by == 'second':
+    schedule.every(number).seconds.do(job)
 
-        except Exception as e:
-            print(e)
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8000, debug=True)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
+
+
+
+
+
